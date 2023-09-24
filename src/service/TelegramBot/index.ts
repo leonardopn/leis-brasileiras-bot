26d27ccb17c108fbc3c41e615adaac85edb1d08e
Leonardo Petta do Nascimento <leonardocps9@gmail.com>
2023-09-24 20:49:30 -0300

    feat(Service: TelegramBot): :sparkles: Adiciona uma mensagem de boas vindas simples se alguém enviar /start

--- src/service/TelegramBot/index.ts
+++ src/service/TelegramBot/index.ts
@@ -26,9 +26,19 @@ export class SingletonTelegramBot extends TelegramBot {
 export function startBot() {
     let errorCount = 0;
     const bot = SingletonTelegramBot.getInstance();
+    logging("Bot do telegram iniciado.");
 
     bot.on("text", async (msg) => {
         try {
+            if (msg.text === "/start") {
+                bot.sendMessage(
+                    msg.chat.id,
+                    "Seja bem vindo ao bot <b>Leis Brasileiras</b>. Estou aqui para te fornecer um acervo sobre todas as leis do Brasil.\n\nPara começar, basta digitar o número da lei e te trarei um arquivo PDF com seu conteúdo.",
+                    { parse_mode: "HTML" }
+                );
+                return;
+            }
+
             const filePath = path.join(__dirname, "..", "..", "temp", `${v4()}.pdf`);
             const isCompleted = await downloadPlanaltoLaw(msg.text || "", filePath, msg.chat.id);
 

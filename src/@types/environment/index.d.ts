9779eaf579f38d3e301c889cc94e2d7dd3aebba5
Leonardo Petta do Nascimento <leonardocps9@gmail.com>
2023-09-25 09:25:07 -0300

    feat: :sparkles: Permite desligar o sistema baseado em variaveis de ambiente

--- src/@types/environment/index.d.ts
+++ src/@types/environment/index.d.ts
@@ -2,6 +2,7 @@ export type DefaultEnvs = {
     NODE_ENV: "development" | "production";
     TELEGRAM_BOT_TOKEN: string;
     NTBA_FIX_350: 1 | 0;
+    STOP_APP: boolean;
 };
 
 declare global {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import "dotenv/config";
 import "./schedules";
 import { startBot } from "./service/TelegramBot";
+import { logging } from "./utils/logger";
 
 startBot();
+
+if (process.env.STOP_APP) {
+    logging("Sistema foi solicitado a desligar.");
+    process.exit(0);
+}
